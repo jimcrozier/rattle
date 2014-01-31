7ec97b88f0fcfbbd8b67d418d34ac46429d616a3
yanqian325 <keaneding@gmail.com>
2014-01-31 13:28:29 -0500

    Create wilcoxon signed rank.R

---
+++ Test/Paired-Two-Sample-Tests/wilcoxon signed rank.R
@@ -0,0 +1,4 @@
+# Use the fBasics package for statistical tests.
+require(fBasics, quietly=TRUE)
+# Perform the test.
+wilcox.test(na.omit(crs$dataset[, "Sepal.Length"]), na.omit(crs$dataset[, "Petal.Length"]), paired=TRUE)
