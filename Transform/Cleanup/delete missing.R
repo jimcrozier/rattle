5a76b54c71a5ac03d0d5e18da87fc54bf940e1de
yanqian325 <keaneding@gmail.com>
2014-02-03 10:48:45 -0500

    Create delete missing.R
    Delete any variable with any missing values.

---
+++ Transform/Cleanup/delete missing.R
@@ -0,0 +1,15 @@
+# Remove specific variables from the dataset.
+crs$dataset$V1 <- NULL
+crs$dataset$V2 <- NULL
+crs$dataset$V3 <- NULL
+# Rattle timestamp: 2014-02-03 10:33:00 i386-w64-mingw32 
+# Note the user selections. 
+# The following variable selections have been noted.
+crs$input <- NULL
+crs$numeric <- NULL
+crs$categoric <- NULL
+crs$target  <- NULL
+crs$risk    <- NULL
+crs$ident   <- NULL
+crs$ignore  <- NULL
+crs$weights <- NULL
