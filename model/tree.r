05d9320f7e98a722d58dab5a24c1df1c30f0195c
jimcrozier <crozierrj@gmail.com>
2014-02-06 14:03:30 -0500

    Create tree.r

---
+++ model/tree.r
@@ -0,0 +1,29 @@
+#============================================================
+# Rattle timestamp: 2014-02-06 14:02:29 x86_64-w64-mingw32 
+
+# Decision Tree 
+
+# The 'rpart' package provides the 'rpart' function.
+
+require(rpart, quietly=TRUE)
+
+# Reset the random number seed to obtain the same results each time.
+
+set.seed(crv$seed)
+
+# Build the Decision Tree model.
+
+crs$rpart <- rpart(Species ~ .,
+    data=crs$dataset[crs$train, c(crs$input, crs$target)],
+    method="class",
+    parms=list(split="information"),
+    control=rpart.control(usesurrogate=0, 
+        maxsurrogate=0))
+
+# Generate a textual view of the Decision Tree model.
+
+print(crs$rpart)
+printcp(crs$rpart)
+cat("\n")
+
+# Time taken: 0.01 secs
