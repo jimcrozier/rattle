7864e2c5911f8025fab61fd30e09a40d4b83e67d
yuexiu920 <yuexiu920@gmail.com>
2014-02-14 12:52:59 -0500

    Create precision.R

---
+++ Evaluate/precision.R
@@ -0,0 +1,22 @@
+# Evaluate model performance. 
+# Precision/Recall Plot: requires the ROCR package
+require(ROCR, quietly=TRUE)
+# Generate a Precision/Recall Plot for the glm model on tongue [validate].
+crs$pr <- predict(crs$glm, type="response", newdata=crs$dataset[crs$validate, c(crs$input, crs$target)])
+# Deal with any missing values in the target variable by 
+# ignoring any training data with missing target values.
+no.miss <- na.omit(crs$dataset[crs$validate, c(crs$input, crs$target)]$delta)
+miss.list <- attr(no.miss, "na.action")
+attributes(no.miss) <- NULL
+if (length(miss.list)) {
+	pred <- prediction(crs$pr[-miss.list], no.miss)
+} else {
+	pred <- prediction(crs$pr, no.miss)
+}
+plot(performance(pred, "prec", "rec"), col="#CC0000FF", lty=1, add=FALSE)
+# Add a legend to the plot.
+legend("bottomleft", c("glm"), col=rainbow(1, 1, .8), lty=1:1, title="Models", inset=c(0.05, 0.05))
+# Add decorations to the plot.
+title(main="Precision/Recall Plot  tongue [validate]",
+    sub=paste("Rattle", format(Sys.time(), "%Y-%b-%d %H:%M:%S"), Sys.info()["user"]))
+grid()
